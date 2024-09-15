c10460f3288884e4fc95e19a342837803f43b179
Lillian Wolfson <lillywolf@gmail.com>
2024-09-14 20:22:18 -0400

    update elements.mjs to call itself on error

--- src/elements.mjs
+++ src/elements.mjs
@@ -4,18 +4,17 @@ import XmlStream from 'xml-stream-saxjs';
 import sax from 'sax';
 import * as db from './db.mjs';
 
-async function osm() {
-  const filename = process.argv[2];
-  const readableStream = createReadStream(path.join(import.meta.dirname, `./data/${filename}`), { objectMode: true, highWaterMark: 500 });
-  const saxStream = sax.createStream(true);
-  const xmlStream = new XmlStream(readableStream);
+async function streamData({
+  xmlStream,
+  filename,
+  currentByte
+}) {
   const sql = await db.connect();
-
-  let counter = 0;
 
   xmlStream
     .on('error', (e) => {
-      console.error(`ERROR: elements - sax stream error in file ${filename}`, e);
+      console.error(`ERROR: elements - sax stream error in file ${filename} at byte ${currentByte}`, e);
+      osm(currentByte);
     })
     .on('endElement: node', (node) => {
       console.log(`UPSERT NODE: upsert node id ${node.$.id}`, filename);
@@ -75,10 +74,27 @@ async function osm() {
         ],
     });
   });
+}
+
+async function osm(byte) {
+  let counter = 0;
+  let currentByte = byte || 0;
+
+  const filename = process.argv[2];
+  const readableStream = createReadStream(path.join(import.meta.dirname, `./data/${filename}`), { objectMode: true, highWaterMark: 500, start: currentByte });
+  const saxStream = sax.createStream(true);
+  const xmlStream = new XmlStream(readableStream);
+
+  streamData({
+    xmlStream,
+    filename,
+    currentByte,
+  });
 
   readableStream
     .pipe(saxStream)
     .on('data', (chunk) => {
+      currentByte += chunk.length;
       console.log('CHUNK: elements', counter.toString(), filename);
       readableStream.pause();
       setTimeout(() => {
@@ -87,7 +103,8 @@ async function osm() {
       }, 10);
     })
     .on('error', (e) => {
-      console.error(`ERROR: elements - stream read error in file ${filename}`, e);
+      console.error(`ERROR: elements - stream read error in file ${filename} at byte ${currentByte}`, e);
+      osm(currentByte);
     })
     .on('end', () => {
       console.log(`COMPLETE: elements - stream processing complete for file ${filename}`);
